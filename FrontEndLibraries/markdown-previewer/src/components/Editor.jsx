a12fec15f912492e56270e9c5766a9802e9ff8bd
Patrick Crawford <pwc.crawford@gmail.com>
2019-06-06 18:01:12 -0400

    finished markdown previewer

--- FrontEndLibraries/markdown-previewer/src/components/Editor.jsx
+++ FrontEndLibraries/markdown-previewer/src/components/Editor.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
-import '../App.css'
-import Toolbar from './Toolbar.jsx'
+import React, { Component } from "react";
+import "../App.css";
+import Toolbar from "./Toolbar.jsx";
 
 class Editor extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    render() {
-        return (
-            <div>
-                <Toolbar text="Editor" />
-                <textarea id="editor" />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className={this.props.className}>
+				<Toolbar
+					text="Editor"
+					className="toolbar"
+					onMax={this.props.onMax}
+					isMax={this.props.isMax}
+				/>
+				<textarea
+					id="editor"
+					value={this.props.text}
+					type="text"
+					onChange={this.props.onChange}
+				/>
+			</div>
+		);
+	}
 }
 
-export default Editor
+export default Editor;
