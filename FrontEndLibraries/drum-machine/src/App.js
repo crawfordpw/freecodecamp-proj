4c376524bfdb5f35c39cfeaacbfe271acbbaa175
Patrick Crawford <pwc.crawford@gmail.com>
2019-06-07 00:25:22 -0400

    finished styling. added power and volume controls

--- FrontEndLibraries/drum-machine/src/App.js
+++ FrontEndLibraries/drum-machine/src/App.js
@@ -1,68 +1,91 @@
-import React, { Component } from "react";
-import "./App.css";
-import DrumPad from "./components/drumpad.jsx";
+import React, { Component } from 'react';
+import './App.scss';
+import DrumPad from './components/drumpad.jsx';
 
 const dataBank = [
 	{
-		id: "Chord-1",
-		letter: "Q",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+		id: 'Chord-1',
+		letter: 'Q',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
 	},
 	{
-		id: "Chord-2",
-		letter: "W",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+		id: 'Chord-2',
+		letter: 'W',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
 	},
 	{
-		id: "Chord-3",
-		letter: "E",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+		id: 'Chord-3',
+		letter: 'E',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
 	},
 	{
-		id: "Shaker",
-		letter: "A",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+		id: 'Shaker',
+		letter: 'A',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
 	},
 	{
-		id: "Open-HH",
-		letter: "S",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+		id: 'Open-HH',
+		letter: 'S',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
 	},
 	{
-		id: "Closed-HH",
-		letter: "D",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+		id: 'Closed-HH',
+		letter: 'D',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
 	},
 	{
-		id: "Punchy-Kick",
-		letter: "Z",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+		id: 'Punchy-Kick',
+		letter: 'Z',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
 	},
 	{
-		id: "Side-Stick",
-		letter: "X",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+		id: 'Side-Stick',
+		letter: 'X',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
 	},
 	{
-		id: "Snare",
-		letter: "C",
-		src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+		id: 'Snare',
+		letter: 'C',
+		src: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
 	}
 ];
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { display: " " };
+		this.state = { display: 'ON', power: true, slider: 0.5 };
 	}
 
+	adjustVolume = e => {
+		if (this.state.power) {
+			this.setState({
+				slider: e.target.value,
+				display: 'Volume: ' + Math.round(e.target.value * 100)
+			});
+		}
+	};
+
 	handleDisplay = display => {
-		this.setState({ display });
+		this.setState({ display: display.replace(/-/g, ' ') });
+	};
+
+	handlePower = () => {
+		this.setState({ power: !this.state.power });
+		if (!this.state.power) {
+			this.setState({ display: 'ON' });
+		} else {
+			this.setState({ display: 'OFF' });
+		}
 	};
 
 	render() {
+		const power = this.state.power
+			? ['select-on', 'select-inner-on']
+			: ['select-off', 'select-inner-off'];
+
 		return (
 			<div className="App">
+				<h1>Drum Machine</h1>
 				<div id="drum-machine" className="drum-panel">
 					<div className="drum-pads">
 						{dataBank.map(data => (
@@ -72,11 +95,33 @@ class App extends Component {
 								src={data.src}
 								letter={data.letter}
 								handleDisplay={this.handleDisplay}
+								power={this.state.power}
+								volume={this.state.slider}
 							/>
 						))}
 					</div>
 					<div className="right-panel">
 						<div id="display">{this.state.display}</div>
+						<div className="controls">
+							<div onClick={this.handlePower}>
+								<div className={power[0]}>
+									<div className={power[1]} />
+								</div>
+								<p>Power</p>
+							</div>
+							<div>
+								<div className="volume-slider">
+									<input
+										type="range"
+										min="0"
+										max="1"
+										step="0.01"
+										value={this.state.slider}
+										onChange={this.adjustVolume}
+									/>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
--- FrontEndLibraries/drum-machine/src/components/drumpad.jsx
+++ FrontEndLibraries/drum-machine/src/components/drumpad.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import "../App.css";
+import React, { Component } from 'react';
+import '../App.scss';
 
 class DrumPad extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = { active: false };
 	}
 
 	componentDidMount() {
-		document.addEventListener("keydown", this.handleKeyPress);
+		document.addEventListener('keydown', this.handleKeyPress);
 	}
 
 	componentWillUnmount() {
-		document.removeEventListener("keydown", this.handleKeyPress);
+		document.removeEventListener('keydown', this.handleKeyPress);
 	}
 
 	handleKeyPress = e => {
@@ -21,17 +21,30 @@ class DrumPad extends Component {
 		}
 	};
 
+	activate = () => {
+		this.setState({
+			active: !this.state.active
+		});
+	};
+
 	playSound = e => {
-		this.props.handleDisplay(this.props.id);
-		const sound = document.getElementById(this.props.letter);
-		sound.currentTime = 0;
-		sound.play();
+		if (this.props.power) {
+			this.props.handleDisplay(this.props.id);
+			const sound = document.getElementById(this.props.letter);
+			sound.currentTime = 0;
+			sound.volume = this.props.volume;
+			sound.play();
+			this.activate();
+			setTimeout(() => this.activate(), 100);
+		}
 	};
 
 	render() {
+		const drumPad = this.state.active ? 'drum-pad-hit' : 'drum-pad';
+
 		return (
 			<div
-				className="drum-pad"
+				className={drumPad}
 				id={this.props.id}
 				onClick={this.playSound}
 			>
