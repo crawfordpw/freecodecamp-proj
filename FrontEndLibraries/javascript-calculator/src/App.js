45ae7a2d9a5b428829ef9e1c8e2139ab9d6899d3
crawfordpw <pwc.crawford@gmail.com>
2019-06-07 15:48:25 -0400

    added calc buttons. started styling

--- FrontEndLibraries/javascript-calculator/src/App.js
+++ FrontEndLibraries/javascript-calculator/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react'
+import './App.scss'
+import Buttons from './components/buttons'
 
-function App() {
-    return <div className="App" />
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="calculator">
+                    <div id="display">asd</div>
+                    <Buttons />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
