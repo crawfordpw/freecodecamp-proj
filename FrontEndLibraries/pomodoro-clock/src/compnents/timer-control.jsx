fde3c88aaade524e17c99b9f8125807b8ca42eab
Patrick Crawford <pwc.crawford@gmail.com>
2019-06-08 16:37:52 -0400

    most basic styling in place

--- FrontEndLibraries/pomodoro-clock/src/compnents/timer-control.jsx
+++ FrontEndLibraries/pomodoro-clock/src/compnents/timer-control.jsx
@@ -1,16 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class TimerControl extends Component {
-    render() {
-        return (
-            <div>
-                <div id={this.props.labelID}>{this.props.label}</div>
-                <div id={this.props.decrement} />
-                <div id={this.props.lengthID}>{this.props.length}</div>
-                <div id={this.props.increment} />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div id={this.props.labelID} className="control-label">
+					{this.props.label}
+				</div>
+				<div className="inc-dec">
+					<button
+						id={this.props.decrement}
+						value="-"
+						onClick={this.props.onClick}
+						className="btns">
+						<i className="fas fa-arrow-down arrows" />
+					</button>
+					<div id={this.props.lengthID}>{this.props.length}</div>
+					<button
+						id={this.props.increment}
+						value="+"
+						onClick={this.props.onClick}
+						className="btns">
+						<i className="fas fa-arrow-up arrows" />
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default TimerControl
+export default TimerControl;
--- FrontEndLibraries/pomodoro-clock/src/compnents/timer.jsx
+++ FrontEndLibraries/pomodoro-clock/src/compnents/timer.jsx
@@ -1,43 +1,77 @@
-import React, { Component } from 'react'
-import '../App.scss'
-import TimerControl from './timer-control.jsx'
+import React, { Component } from 'react';
+import '../App.scss';
+import TimerControl from './timer-control.jsx';
 
 class Timer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            breakLength: 5,
-            sessionLength: 25,
-            timeLeft: '15:00',
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			breakLength: 5,
+			sessionLength: 25,
+			timeLeft: '15:00',
+			pause: true
+		};
+	}
 
-    render() {
-        return (
-            <div>
-                <TimerControl
-                    labelID="break-label"
-                    label="Break Length"
-                    decrement="break-decrement"
-                    lengthID="break-length"
-                    length={this.state.breakLength}
-                    increment="break-increment"
-                />
-                <TimerControl
-                    labelID="session-label"
-                    label="Session Length"
-                    decrement="session-decrement"
-                    lengthID="session-length"
-                    length={this.state.sessionLength}
-                    increment="session-increment"
-                />
-                <div id="timer-label">Session</div>
-                <div id="time-left">{this.state.timeLeft}</div>
-                <div id="start_stop" />
-                <div id="reset" />
-            </div>
-        )
-    }
+	setBreakLength = e => {
+		this.handleLength('break', e.currentTarget.value);
+	};
+
+	setSessionLength = e => {
+		this.handleLength('session', e.currentTarget.value);
+	};
+
+	handleLength(type, changeVal) {
+		console.log(`Handle Length: ${type}, ${changeVal}`);
+	}
+
+	render() {
+		const classes = this.state.pause
+			? 'fas fa-play play-undo'
+			: 'fas fa-pause play-undo';
+
+		return (
+			<div className="Timer">
+				<h1>Pomodoro Clock</h1>
+				<div className="timer-control">
+					<div className="single-timer-control">
+						<TimerControl
+							labelID="break-label"
+							label="Break Length"
+							decrement="break-decrement"
+							lengthID="break-length"
+							length={this.state.breakLength}
+							increment="break-increment"
+							onClick={this.setBreakLength}
+						/>
+					</div>
+					<div className="single-timer-control">
+						<TimerControl
+							labelID="session-label"
+							label="Session Length"
+							decrement="session-decrement"
+							lengthID="session-length"
+							length={this.state.sessionLength}
+							increment="session-increment"
+							onClick={this.setSessionLength}
+						/>
+					</div>
+				</div>
+				<div className="time-remaining">
+					<div id="timer-label">Session</div>
+					<div id="time-left">{this.state.timeLeft}</div>
+				</div>
+				<div className="control-buttons">
+					<button id="start_stop" className="btns">
+						<i className={classes} />
+					</button>
+					<button id="reset" className="btns">
+						<i className="fas fa-undo play-undo" />
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Timer
+export default Timer;
